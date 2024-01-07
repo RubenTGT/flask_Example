95f0fe6b3267eb253e8c480153830abf7dccb965
Ruben Leal <ruben_neg@outlook.com>
2024-01-07 14:40:59 -0600

    🔐 security: add env variables to avoid hardcode
    Add env variables by using python-decouple

--- app/database/db_connection.py
+++ app/database/db_connection.py
@@ -1,9 +1,10 @@
 from app import app
+from decouple import config
 from flask_mysqldb import MySQL
 
-app.config['MYSQL_USER'] = 'flask_project'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_DB'] = 'flask_project'
+app.config['MYSQL_USER'] = config('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = config('MYSQL_PASSWORD')
+app.config['MYSQL_HOST'] = config('MYSQL_HOST')
+app.config['MYSQL_DB'] = config('MYSQL_DB')
 
 mysql = MySQL(app)
