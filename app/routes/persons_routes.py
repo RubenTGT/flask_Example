ae13817a2cdcef20e8faba25e844a6e4d7ab2082
Ruben Leal <ruben_neg@outlook.com>
2024-01-07 14:02:57 -0600

    ✨ feat: add microservice funcionality
    Add service to search persons by name and last in a database table

--- app/routes/persons_routes.py
+++ app/routes/persons_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from services.persons_service import search_person
 
 persons = Blueprint('contacts', __name__, template_folder='app/templates')
 
@@ -6,3 +7,8 @@ persons = Blueprint('contacts', __name__, template_folder='app/templates')
 @persons.route('/')
 def Index():
     return render_template('index.html')
+
+
+@persons.route('/browse_person', methods=['POST'])
+def search_person_route():
+    return search_person()
---
+++ app/services/persons_service.py
@@ -0,0 +1,24 @@
+from flask import jsonify, request
+from database.db_connection import mysql
+from datetime import datetime
+
+def search_person():
+    if request.method == 'POST':
+        name = request.form['name']
+        last_name = request.form['last_name']
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM personal_info WHERE first_name = %s AND last_name = %s', (name, last_name))
+        result = cur.fetchall()
+
+        response = {"encontrado": len(result) > 0}
+
+        log_query(datetime.now(), f"Name: {name}, Last Name: {last_name}", str(response))
+
+        return jsonify(response)
+
+def log_query(query_date, request_parameters, result):
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO query_log (query_date, request_parameters, result) VALUES (%s, %s, %s)',
+                (query_date, request_parameters, result))
+    mysql.connection.commit()
+    cur.close()
